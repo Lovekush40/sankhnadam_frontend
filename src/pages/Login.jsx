c197c8276243c057c6371def6bd78e333562ea55
Lovekush40 <l.kprajapati9611@gmail.com>
2026-03-31 19:51:35 +0530

    no cahnges

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import peacock_feather from "@/assets/peacock_feather.png";
 import ScrollReveal from "@/components/ScrollReveal";
-import { useAuth } from "@/contexts/AuthContext";
+import { useAuth } from "@/contexts/AuthContext"
 
 const Login = () => {
   const { login, isAuthenticated, isLoading } = useAuth();
